e1bb18564dc2bb080e17c9a653bd3fb3888cfbc7
RuoYi <yzz_ivy@163.com>
2018-03-01 17:21:41 +0800

    初始化信息修改

--- doc/new_intall_20180228.sql
+++ doc/new_intall_20180228.sql
@@ -49,8 +49,8 @@ create table sys_user (
 -- ----------------------------
 -- 初始化-用户信息表数据
 -- ----------------------------
-insert into sys_user values('1', '1', 'admin', '阳宗专', 'yzz_ivy@163.com', '15017911213', '172eee54aa664e9dd0536b063796e54e', '', 0, '维护中', '2018-01-01');
-insert into sys_user values('2', '1', 'ry',    '阳若依', 'ry@163.com',      '15220051213', '2f59d63eddd54f3977d6fe25aec8b2bc', '', 1, '锁定中', '2018-01-01');
+insert into sys_user values('1', '1', 'admin', '若依', 'yzz_ivy@163.com', '15088888888', '172eee54aa664e9dd0536b063796e54e', '', 0, '维护中', '2018-01-01');
+insert into sys_user values('2', '1', 'ry',    '若依', 'ry@163.com',      '15288888888', '2f59d63eddd54f3977d6fe25aec8b2bc', '', 1, '锁定中', '2018-01-01');
 
 
 
