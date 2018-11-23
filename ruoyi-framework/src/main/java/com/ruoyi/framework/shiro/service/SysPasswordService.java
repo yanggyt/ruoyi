d7d50643c82b9ca31f008d478d5442f2290af472
zhaocongcong <zhaocongcong@youyuwo.com>
2018-11-23 16:08:05 +0800

    去除测试代码

--- ruoyi-framework/src/main/java/com/ruoyi/framework/shiro/service/SysPasswordService.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/shiro/service/SysPasswordService.java
@@ -1,13 +1,5 @@
 package com.ruoyi.framework.shiro.service;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import javax.annotation.PostConstruct;
-import org.apache.shiro.cache.Cache;
-import org.apache.shiro.cache.CacheManager;
-import org.apache.shiro.crypto.hash.Md5Hash;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import com.ruoyi.common.constant.Constants;
 import com.ruoyi.framework.manager.AsyncManager;
 import com.ruoyi.framework.manager.factory.AsyncFactory;
@@ -15,15 +7,23 @@ import com.ruoyi.framework.util.MessageUtils;
 import com.ruoyi.framework.web.exception.user.UserPasswordNotMatchException;
 import com.ruoyi.framework.web.exception.user.UserPasswordRetryLimitExceedException;
 import com.ruoyi.system.domain.SysUser;
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheManager;
+import org.apache.shiro.crypto.hash.Md5Hash;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 登录密码方法
- * 
+ *
  * @author ruoyi
  */
 @Component
-public class SysPasswordService
-{
+public class SysPasswordService {
     @Autowired
     private CacheManager cacheManager;
 
@@ -33,58 +33,43 @@ public class SysPasswordService
     private String maxRetryCount;
 
     @PostConstruct
-    public void init()
-    {
+    public void init() {
         loginRecordCache = cacheManager.getCache("loginRecordCache");
     }
 
-    public void validate(SysUser user, String password)
-    {
+    public void validate(SysUser user, String password) {
         String loginName = user.getLoginName();
 
         AtomicInteger retryCount = loginRecordCache.get(loginName);
 
-        if (retryCount == null)
-        {
+        if (retryCount == null) {
             retryCount = new AtomicInteger(0);
             loginRecordCache.put(loginName, retryCount);
         }
-        if (retryCount.incrementAndGet() > Integer.valueOf(maxRetryCount).intValue())
-        {
+        if (retryCount.incrementAndGet() > Integer.valueOf(maxRetryCount).intValue()) {
             AsyncManager.me().execute(AsyncFactory.recordLogininfor(loginName, Constants.LOGIN_FAIL, MessageUtils.message("user.password.retry.limit.exceed", maxRetryCount)));
             throw new UserPasswordRetryLimitExceedException(Integer.valueOf(maxRetryCount).intValue());
         }
 
-        if (!matches(user, password))
-        {
+        if (!matches(user, password)) {
             AsyncManager.me().execute(AsyncFactory.recordLogininfor(loginName, Constants.LOGIN_FAIL, MessageUtils.message("user.password.retry.limit.count", retryCount)));
             loginRecordCache.put(loginName, retryCount);
             throw new UserPasswordNotMatchException();
-        }
-        else
-        {
+        } else {
             clearLoginRecordCache(loginName);
         }
     }
 
-    public boolean matches(SysUser user, String newPassword)
-    {
+    public boolean matches(SysUser user, String newPassword) {
         return user.getPassword().equals(encryptPassword(user.getLoginName(), newPassword, user.getSalt()));
     }
 
-    public void clearLoginRecordCache(String username)
-    {
+    public void clearLoginRecordCache(String username) {
         loginRecordCache.remove(username);
     }
 
-    public String encryptPassword(String username, String password, String salt)
-    {
+    public String encryptPassword(String username, String password, String salt) {
         return new Md5Hash(username + password + salt).toHex().toString();
     }
 
-    public static void main(String[] args)
-    {
-        System.out.println(new SysPasswordService().encryptPassword("admin", "admin123", "111111"));
-        System.out.println(new SysPasswordService().encryptPassword("ry", "admin123", "222222"));
-    }
 }
