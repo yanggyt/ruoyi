c7ccd80b249949c839efde03fd218e12044fac4b
flower <zloiet@163.com>
2018-12-19 00:13:34 +0800

    问题页面，根父节点不能选，递归显示所有下级节点问题

--- ruoyi-exam/src/main/java/com/ruoyi/exam/controller/ExamQuestionController.java
+++ ruoyi-exam/src/main/java/com/ruoyi/exam/controller/ExamQuestionController.java
@@ -1,7 +1,11 @@
 package com.ruoyi.exam.controller;
 
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+
+import com.ruoyi.exam.domain.ExamQuestionCategory;
+import com.ruoyi.exam.service.IExamQuestionCategoryService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -31,6 +35,9 @@ public class ExamQuestionController extends BaseController
 	
 	@Autowired
 	private IExamQuestionService examQuestionService;
+
+	@Autowired
+	private IExamQuestionCategoryService examQuestionCategoryService;
 	
 	@RequiresPermissions("exam:examQuestion:view")
 	@GetMapping()
@@ -71,8 +78,28 @@ public class ExamQuestionController extends BaseController
 	@ResponseBody
 	public TableDataInfo list(ExamQuestion examQuestion)
 	{
-        List<ExamQuestion> list = examQuestionService.selectExamQuestionList(examQuestion);
-		return getDataTable(list);
+		ArrayList<ExamQuestion> objects = new ArrayList<>();
+		List<ExamQuestion> listByIds = getListByIds(objects, examQuestion);
+		return getDataTable(listByIds);
+	}
+
+	/**
+	 * 递归找到所有下级的题目
+	 * @param list
+	 * @param examQuestion
+	 * @return
+	 */
+	private List<ExamQuestion> getListByIds(List<ExamQuestion> list,ExamQuestion examQuestion){
+		list.addAll(examQuestionService.selectExamQuestionList(examQuestion));
+		String categoryId = examQuestion.getCategoryId();
+		ExamQuestionCategory examQuestionCategory = new ExamQuestionCategory();
+		examQuestionCategory.setParentId(Long.parseLong(categoryId));
+		List<ExamQuestionCategory> examQuestionCategories = examQuestionCategoryService.selectList(examQuestionCategory);
+		for (ExamQuestionCategory questionCategory : examQuestionCategories) {
+			examQuestion.setCategoryId(questionCategory.getId().toString());
+			getListByIds(list,examQuestion);
+		}
+		return list;
 	}
 	
 	
