f08bb2c98d4bb64e09e221c5616532ff18095712
flower <zloiet@163.com>
2018-12-18 22:59:33 +0800

    试卷分类扩展层级不卡控

--- ruoyi-exam/src/main/java/com/ruoyi/exam/controller/ExamQuestionCategoryController.java
+++ ruoyi-exam/src/main/java/com/ruoyi/exam/controller/ExamQuestionCategoryController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Map;
 
 import com.ruoyi.exam.domain.ExamPaperCategory;
+import com.ruoyi.exam.domain.ExamQuestion;
+import com.ruoyi.exam.service.IExamQuestionService;
 import com.ruoyi.framework.web.util.ShiroUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,169 +35,159 @@ import com.ruoyi.common.utils.ExcelUtil;
  */
 @Controller
 @RequestMapping("/exam/examQuestionCategory")
-public class ExamQuestionCategoryController extends BaseController
-{
-	private String prefix = "exam/examQuestionCategory";
+public class ExamQuestionCategoryController extends BaseController {
+    private String prefix = "exam/examQuestionCategory";
 
-	@Autowired
-	private IExamQuestionCategoryService examQuestionCategoryService;
+    @Autowired
+    private IExamQuestionCategoryService examQuestionCategoryService;
 
-	@RequiresPermissions("exam:examQuestionCategory:view")
-	@GetMapping()
-	public String examQuestionCategory()
-	{
-		return prefix + "/examQuestionCategory";
-	}
+    @Autowired
+    private IExamQuestionService examQuestionService;
 
-
-
-	/**
-	 * 查询试题分类列表
-	 */
-	@RequiresPermissions("exam:examQuestionCategory:list")
-	@GetMapping("/list")
-	@ResponseBody
-	public List<ExamQuestionCategory> list(ExamQuestionCategory examQuestionCategory)
-	{
-
-		List<ExamQuestionCategory> list = examQuestionCategoryService.selectExamQuestionCategoryList(examQuestionCategory);
-		return list;
-	}
+    @RequiresPermissions("exam:examQuestionCategory:view")
+    @GetMapping()
+    public String examQuestionCategory() {
+        return prefix + "/examQuestionCategory";
+    }
 
 
-	/**
-	 * 导出试题分类列表
-	 */
-	@RequiresPermissions("exam:examQuestionCategory:export")
-	@PostMapping("/export")
-	@ResponseBody
-	public AjaxResult export(ExamQuestionCategory examQuestionCategory)
-	{
-		List<ExamQuestionCategory> list = examQuestionCategoryService.selectExamQuestionCategoryList(examQuestionCategory);
-		ExcelUtil<ExamQuestionCategory> util = new ExcelUtil<ExamQuestionCategory>(ExamQuestionCategory.class);
-		return util.exportExcel(list, "examQuestionCategory");
-	}
+    /**
+     * 查询试题分类列表
+     */
+    @RequiresPermissions("exam:examQuestionCategory:list")
+    @GetMapping("/list")
+    @ResponseBody
+    public List<ExamQuestionCategory> list(ExamQuestionCategory examQuestionCategory) {
 
-	/**
-	 * 新增试题分类
-	 */
-	@GetMapping("/add/{parentId}")
-	public String add(@PathVariable("parentId") String parentId, ModelMap mmap)
-	{
-		mmap.put("examQuestionCategory", examQuestionCategoryService.selectExamQuestionCategoryById(parentId));
-		return prefix + "/add";
-	}
-
-	/**
-	 * 新增保存试题分类
-	 */
-	@RequiresPermissions("exam:examQuestionCategory:add")
-	@Log(title = "试题分类", businessType = BusinessType.INSERT)
-	@PostMapping("/add")
-	@ResponseBody
-	public AjaxResult addSave(ExamQuestionCategory examQuestionCategory)
-	{
-		examQuestionCategory.setCreateBy(ShiroUtils.getLoginName());
-		examQuestionCategory.setCreateDate(new Date());
-		return toAjax(examQuestionCategoryService.insertExamQuestionCategory(examQuestionCategory));
-	}
-
-	/**
-	 * 修改试题分类
-	 */
-	@GetMapping("/edit/{id}")
-	public String edit(@PathVariable("id") String id, ModelMap mmap)
-	{
-		ExamQuestionCategory examQuestionCategory = examQuestionCategoryService.selectExamQuestionCategoryById(id);
-		mmap.put("examQuestionCategory", examQuestionCategory);
-		return prefix + "/edit";
-	}
-
-	/**
-	 * 修改保存试题分类
-	 */
-	@RequiresPermissions("exam:examQuestionCategory:edit")
-	@Log(title = "试题分类", businessType = BusinessType.UPDATE)
-	@PostMapping("/edit")
-	@ResponseBody
-	public AjaxResult editSave(ExamQuestionCategory examQuestionCategory)
-	{
-		return toAjax(examQuestionCategoryService.updateExamQuestionCategory(examQuestionCategory));
-	}
-
-	/**
-	 * 删除试题分类
-	 */
-	@RequiresPermissions("exam:examQuestionCategory:remove")
-	@Log(title = "试题分类", businessType = BusinessType.DELETE)
-	@PostMapping( "/remove")
-	@ResponseBody
-	public AjaxResult remove(String ids)
-	{
-		return toAjax(examQuestionCategoryService.deleteExamQuestionCategoryByIds(ids));
-	}
+        List<ExamQuestionCategory> list = examQuestionCategoryService.selectExamQuestionCategoryList(examQuestionCategory);
+        return list;
+    }
 
 
-	/**
-	 * 选择部门树
-	 */
-	@GetMapping("/selectExamQuestionCategoryTree/{examQuestionCategoryId}")
-	public String selectDeptTree(@PathVariable("examQuestionCategoryId") String examQuestionCategoryId, ModelMap mmap)
-	{
-		mmap.put("examQuestionCategory", examQuestionCategoryService.selectExamQuestionCategoryById(examQuestionCategoryId));
-		return prefix + "/tree";
-	}
+    /**
+     * 导出试题分类列表
+     */
+    @RequiresPermissions("exam:examQuestionCategory:export")
+    @PostMapping("/export")
+    @ResponseBody
+    public AjaxResult export(ExamQuestionCategory examQuestionCategory) {
+        List<ExamQuestionCategory> list = examQuestionCategoryService.selectExamQuestionCategoryList(examQuestionCategory);
+        ExcelUtil<ExamQuestionCategory> util = new ExcelUtil<ExamQuestionCategory>(ExamQuestionCategory.class);
+        return util.exportExcel(list, "examQuestionCategory");
+    }
+
+    /**
+     * 新增试题分类
+     */
+    @GetMapping("/add/{parentId}")
+    public String add(@PathVariable("parentId") String parentId, ModelMap mmap) {
+        mmap.put("examQuestionCategory", examQuestionCategoryService.selectExamQuestionCategoryById(parentId));
+        return prefix + "/add";
+    }
+
+    /**
+     * 新增保存试题分类
+     */
+    @RequiresPermissions("exam:examQuestionCategory:add")
+    @Log(title = "试题分类", businessType = BusinessType.INSERT)
+    @PostMapping("/add")
+    @ResponseBody
+    public AjaxResult addSave(ExamQuestionCategory examQuestionCategory) {
+        ExamQuestion examQuestion = new ExamQuestion();
+        examQuestion.setCategoryId(examQuestionCategory.getParentId().toString());
+        if (examQuestionService.selectList(examQuestion).size() > 0) {
+            return error(1, "分类包含试题，不允许扩展分类");
+        }
+        examQuestionCategory.setCreateBy(ShiroUtils.getLoginName());
+        examQuestionCategory.setCreateDate(new Date());
+        return toAjax(examQuestionCategoryService.insertExamQuestionCategory(examQuestionCategory));
+    }
+
+    /**
+     * 修改试题分类
+     */
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") String id, ModelMap mmap) {
+        ExamQuestionCategory examQuestionCategory = examQuestionCategoryService.selectExamQuestionCategoryById(id);
+        mmap.put("examQuestionCategory", examQuestionCategory);
+        return prefix + "/edit";
+    }
+
+    /**
+     * 修改保存试题分类
+     */
+    @RequiresPermissions("exam:examQuestionCategory:edit")
+    @Log(title = "试题分类", businessType = BusinessType.UPDATE)
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(ExamQuestionCategory examQuestionCategory) {
+        return toAjax(examQuestionCategoryService.updateExamQuestionCategory(examQuestionCategory));
+    }
+
+    /**
+     * 删除试题分类
+     */
+    @RequiresPermissions("exam:examQuestionCategory:remove")
+    @Log(title = "试题分类", businessType = BusinessType.DELETE)
+    @PostMapping("/remove")
+    @ResponseBody
+    public AjaxResult remove(String ids) {
+        return toAjax(examQuestionCategoryService.deleteExamQuestionCategoryByIds(ids));
+    }
 
 
-	/**
-	 * 加载列表树
-	 */
-	@GetMapping("/treeData")
-	@ResponseBody
-	public List<Map<String, Object>> treeData()
-	{
-		List<Map<String, Object>> tree = examQuestionCategoryService.selectDeptTree();
-		return tree;
-	}
+    /**
+     * 选择部门树
+     */
+    @GetMapping("/selectExamQuestionCategoryTree/{examQuestionCategoryId}")
+    public String selectDeptTree(@PathVariable("examQuestionCategoryId") String examQuestionCategoryId, ModelMap mmap) {
+        mmap.put("examQuestionCategory", examQuestionCategoryService.selectExamQuestionCategoryById(examQuestionCategoryId));
+        return prefix + "/tree";
+    }
 
 
-	/**
-	 * 选择部门树
-	 */
-	@GetMapping("/treeDataForAdd")
-	@ResponseBody
-	public List<Map<String, Object>> treeDataForAdd()
-	{
-		List<Map<String, Object>> tree = examQuestionCategoryService.selectDeptTree();
-		List<Map<String, Object>> res = new ArrayList<>();
-		for (Map<String, Object> stringObjectMap : tree) {
-			String pId = stringObjectMap.get("pId").toString();
-			if(pId.equals("0")||pId.equals("1")){
-				res.add(stringObjectMap);
-			}
+    /**
+     * 加载列表树
+     */
+    @GetMapping("/treeData")
+    @ResponseBody
+    public List<Map<String, Object>> treeData() {
+        List<Map<String, Object>> tree = examQuestionCategoryService.selectDeptTree();
+        return tree;
+    }
 
-		}
 
-		return res;
-	}
+    /**
+     * 选择部门树
+     */
+    @GetMapping("/treeDataForAdd")
+    @ResponseBody
+    public List<Map<String, Object>> treeDataForAdd() {
+        List<Map<String, Object>> tree = examQuestionCategoryService.selectDeptTree();
+        return tree;
+    }
 
-	/**
-	 * 删除
-	 */
-	@Log(title = "试题分类管理", businessType = BusinessType.DELETE)
-	@RequiresPermissions("system:examQuestionCategory:remove")
-	@PostMapping("/remove/{id}")
-	@ResponseBody
-	public AjaxResult remove(@PathVariable("id") Long id)
-	{
-		ExamQuestionCategory exam = new ExamQuestionCategory();
-		exam.setParentId(id);
-		if (examQuestionCategoryService.selectList(exam).size() > 0)
-		{
-			return error(1, "存在下级分类,不允许删除");
-		}
+    /**
+     * 删除
+     */
+    @Log(title = "试题分类管理", businessType = BusinessType.DELETE)
+    @RequiresPermissions("system:examQuestionCategory:remove")
+    @PostMapping("/remove/{id}")
+    @ResponseBody
+    public AjaxResult remove(@PathVariable("id") Long id) {
+        ExamQuestionCategory exam = new ExamQuestionCategory();
+        exam.setParentId(id);
+        if (examQuestionCategoryService.selectList(exam).size() > 0) {
+            return error(1, "存在下级分类,不允许删除");
+        }
+        ExamQuestion examQuestion = new ExamQuestion();
+        examQuestion.setCategoryId(id.toString());
 
-		return toAjax(examQuestionCategoryService.deleteById(id));
-	}
+        if (examQuestionService.selectList(examQuestion).size() > 0) {
+            return error(1, "分类包含试题，不允许删除");
+        }
+
+        return toAjax(examQuestionCategoryService.deleteById(id));
+    }
 
 }
