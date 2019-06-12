acdf998880c649d2b759618cdebe426e857b10b0
tangpeng <tangpeng@ezsvs.com>
2019-06-12 21:13:56 +0800

    BaseEntity添加ForeignKeyInfo参数供请求手动解析外键

--- ruoyi-system/src/main/java/com/ruoyi/template/domain/TmplServer.java
+++ ruoyi-system/src/main/java/com/ruoyi/template/domain/TmplServer.java
@@ -6,114 +6,121 @@ import com.ruoyi.common.core.domain.BaseEntity;
 
 /**
  * 服务器模板表 tmpl_server
- * 
+ *
  * @author TP
  * @date 2019-06-12
  */
-public class TmplServer extends BaseEntity
-{
-	private static final long serialVersionUID = 1L;
-	
-	/** 服务器模板编号 */
-	private Integer serverId;
-	/** 服务器品牌 */
-	private String serverBrand;
-	/** 服务器型号 */
-	private String serverType;
-	/** CPU主频 */
-	private String cpuFreq;
-	/** CPU数量 */
-	private Integer cpuNum;
-	/** IPMI端口 */
-	private Integer ipmiPort;
-	/** 服务器电源数量 */
-	private Integer powerNum;
-	/** 服务器raid卡 */
-	private String raidCard;
+public class TmplServer extends BaseEntity {
+    private static final long serialVersionUID = 1L;
 
-	public void setServerId(Integer serverId) 
-	{
-		this.serverId = serverId;
-	}
+    /**
+     * 服务器模板编号
+     */
+    private Integer serverId;
+    /**
+     * 服务器品牌
+     */
+    private String serverBrand;
+    /**
+     * 服务器型号
+     */
+    private String serverType;
+    /**
+     * CPU主频
+     */
+    private String cpuFreq;
+    /**
+     * CPU数量
+     */
+    private Integer cpuNum;
+    /**
+     * IPMI端口
+     */
+    private Integer ipmiPort;
+    /**
+     * 服务器电源数量
+     */
+    private Integer powerNum;
+    /**
+     * 服务器raid卡
+     */
+    private String raidCard;
 
-	public Integer getServerId() 
-	{
-		return serverId;
-	}
-	public void setServerBrand(String serverBrand) 
-	{
-		this.serverBrand = serverBrand;
-	}
+    public void setServerId(Integer serverId) {
+        this.serverId = serverId;
+    }
 
-	public String getServerBrand() 
-	{
-		return serverBrand;
-	}
-	public void setServerType(String serverType) 
-	{
-		this.serverType = serverType;
-	}
+    public Integer getServerId() {
+        return serverId;
+    }
 
-	public String getServerType() 
-	{
-		return serverType;
-	}
-	public void setCpuFreq(String cpuFreq) 
-	{
-		this.cpuFreq = cpuFreq;
-	}
+    public void setServerBrand(String serverBrand) {
+        this.serverBrand = serverBrand;
+    }
 
-	public String getCpuFreq() 
-	{
-		return cpuFreq;
-	}
-	public void setCpuNum(Integer cpuNum) 
-	{
-		this.cpuNum = cpuNum;
-	}
+    public String getServerBrand() {
+        return serverBrand;
+    }
 
-	public Integer getCpuNum() 
-	{
-		return cpuNum;
-	}
-	public void setIpmiPort(Integer ipmiPort) 
-	{
-		this.ipmiPort = ipmiPort;
-	}
+    public void setServerType(String serverType) {
+        this.serverType = serverType;
+    }
 
-	public Integer getIpmiPort() 
-	{
-		return ipmiPort;
-	}
-	public void setPowerNum(Integer powerNum) 
-	{
-		this.powerNum = powerNum;
-	}
+    public String getServerType() {
+        return serverType;
+    }
 
-	public Integer getPowerNum() 
-	{
-		return powerNum;
-	}
-	public void setRaidCard(String raidCard) 
-	{
-		this.raidCard = raidCard;
-	}
+    public void setCpuFreq(String cpuFreq) {
+        this.cpuFreq = cpuFreq;
+    }
 
-	public String getRaidCard() 
-	{
-		return raidCard;
-	}
+    public String getCpuFreq() {
+        return cpuFreq;
+    }
 
+    public void setCpuNum(Integer cpuNum) {
+        this.cpuNum = cpuNum;
+    }
+
+    public Integer getCpuNum() {
+        return cpuNum;
+    }
+
+    public void setIpmiPort(Integer ipmiPort) {
+        this.ipmiPort = ipmiPort;
+    }
+
+    public Integer getIpmiPort() {
+        return ipmiPort;
+    }
+
+    public void setPowerNum(Integer powerNum) {
+        this.powerNum = powerNum;
+    }
+
+    public Integer getPowerNum() {
+        return powerNum;
+    }
+
+    public void setRaidCard(String raidCard) {
+        this.raidCard = raidCard;
+    }
+
+    public String getRaidCard() {
+        return raidCard;
+    }
+
+    @Override
     public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("serverId", getServerId())
-            .append("serverBrand", getServerBrand())
-            .append("serverType", getServerType())
-            .append("cpuFreq", getCpuFreq())
-            .append("cpuNum", getCpuNum())
-            .append("ipmiPort", getIpmiPort())
-            .append("powerNum", getPowerNum())
-            .append("raidCard", getRaidCard())
-            .toString();
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("serverId", getServerId())
+                .append("serverBrand", getServerBrand())
+                .append("serverType", getServerType())
+                .append("cpuFreq", getCpuFreq())
+                .append("cpuNum", getCpuNum())
+                .append("ipmiPort", getIpmiPort())
+                .append("powerNum", getPowerNum())
+                .append("raidCard", getRaidCard())
+                .toString();
     }
 }
--- ruoyi-system/src/main/java/com/ruoyi/template/domain/TmplSwitch.java
+++ ruoyi-system/src/main/java/com/ruoyi/template/domain/TmplSwitch.java
@@ -1,71 +1,89 @@
 package com.ruoyi.template.domain;
 
+import com.ruoyi.common.core.domain.BaseEntity;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
-import com.ruoyi.common.core.domain.BaseEntity;
+
+import java.util.List;
 
 /**
  * 交换机模板表 tmpl_switch
- * 
+ *
  * @author TP
  * @date 2019-06-12
  */
-public class TmplSwitch extends BaseEntity
-{
-	private static final long serialVersionUID = 1L;
-	
-	/** 交换机模板编号 */
-	private Integer switchId;
-	/** 交换机品牌 */
-	private String switchBrand;
-	/** 交换机型号 */
-	private String switchType;
-	/** 交换机电源数量 */
-	private Integer powerNum;
+public class TmplSwitch extends BaseEntity {
+    private static final long serialVersionUID = 1L;
 
-	public void setSwitchId(Integer switchId) 
-	{
-		this.switchId = switchId;
-	}
+    /**
+     * 交换机模板编号
+     */
+    private Integer switchId;
+    /**
+     * 交换机品牌
+     */
+    private String switchBrand;
+    /**
+     * 交换机型号
+     */
+    private String switchType;
+    /**
+     * 交换机电源数量
+     */
+    private Integer powerNum;
+    /**
+     * 交换机端口类型
+     */
+    private List<TmplSwitchPort> switchPorts;
 
-	public Integer getSwitchId() 
-	{
-		return switchId;
-	}
-	public void setSwitchBrand(String switchBrand) 
-	{
-		this.switchBrand = switchBrand;
-	}
+    public void setSwitchId(Integer switchId) {
+        this.switchId = switchId;
+    }
 
-	public String getSwitchBrand() 
-	{
-		return switchBrand;
-	}
-	public void setSwitchType(String switchType) 
-	{
-		this.switchType = switchType;
-	}
+    public Integer getSwitchId() {
+        return switchId;
+    }
 
-	public String getSwitchType() 
-	{
-		return switchType;
-	}
-	public void setPowerNum(Integer powerNum) 
-	{
-		this.powerNum = powerNum;
-	}
+    public void setSwitchBrand(String switchBrand) {
+        this.switchBrand = switchBrand;
+    }
 
-	public Integer getPowerNum() 
-	{
-		return powerNum;
-	}
+    public String getSwitchBrand() {
+        return switchBrand;
+    }
 
+    public void setSwitchType(String switchType) {
+        this.switchType = switchType;
+    }
+
+    public String getSwitchType() {
+        return switchType;
+    }
+
+    public void setPowerNum(Integer powerNum) {
+        this.powerNum = powerNum;
+    }
+
+    public Integer getPowerNum() {
+        return powerNum;
+    }
+
+    public List<TmplSwitchPort> getSwitchPorts() {
+        return switchPorts;
+    }
+
+    public void setSwitchPorts(List<TmplSwitchPort> switchPorts) {
+        this.switchPorts = switchPorts;
+    }
+
+    @Override
     public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("switchId", getSwitchId())
-            .append("switchBrand", getSwitchBrand())
-            .append("switchType", getSwitchType())
-            .append("powerNum", getPowerNum())
-            .toString();
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("switchId", getSwitchId())
+                .append("switchBrand", getSwitchBrand())
+                .append("switchType", getSwitchType())
+                .append("powerNum", getPowerNum())
+                .append("switchPorts", getSwitchPorts())
+                .toString();
     }
 }
