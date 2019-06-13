66233446cf4d8af739f23853ceb72b7039b94efd
wrui <icytail@vip.qq.com>
2019-06-13 18:19:13 +0800

    调整班级字样和上级名称

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/SysDept.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SysDept.java
@@ -9,170 +9,148 @@ import com.ruoyi.common.core.domain.BaseEntity;
  * 
  * @author ruoyi
  */
-public class SysDept extends BaseEntity
-{
-    private static final long serialVersionUID = 1L;
+public class SysDept extends BaseEntity {
+	private static final long serialVersionUID = 1L;
 
-    /** 部门ID */
-    private Long deptId;
+	/** 部门ID */
+	private Long deptId;
 
-    /** 父部门ID */
-    private Long parentId;
+	/** 父部门ID */
+	private Long parentId;
 
-    /** 祖级列表 */
-    private String ancestors;
+	/** 祖级列表 */
+	private String ancestors;
 
-    /** 部门名称 */
-    private String deptName;
+	/** 部门名称 */
+	private String deptName;
 
-    /** 显示顺序 */
-    private String orderNum;
+	/** 父部门名称 */
+	private String parentDeptName;
 
-    /** 负责人 */
-    private String leader;
+	/** 显示顺序 */
+	private String orderNum;
 
-    /** 联系电话 */
-    private String phone;
-    
-    /** 邮箱 */
-    private String email;
+	/** 负责人 */
+	private String leader;
 
-    /** 部门状态:0正常,1停用 */
-    private String status;
+	/** 联系电话 */
+	private String phone;
 
-    /** 删除标志（0代表存在 2代表删除） */
-    private String delFlag;
+	/** 邮箱 */
+	private String email;
 
-    /** 父部门名称 */
-    private String parentName;
+	/** 部门状态:0正常,1停用 */
+	private String status;
 
-    public Long getDeptId()
-    {
-        return deptId;
-    }
+	/** 删除标志（0代表存在 2代表删除） */
+	private String delFlag;
 
-    public void setDeptId(Long deptId)
-    {
-        this.deptId = deptId;
-    }
+	/** 父部门名称 */
+	private String parentName;
 
-    public Long getParentId()
-    {
-        return parentId;
-    }
+	public Long getDeptId() {
+		return deptId;
+	}
 
-    public void setParentId(Long parentId)
-    {
-        this.parentId = parentId;
-    }
+	public void setDeptId(Long deptId) {
+		this.deptId = deptId;
+	}
 
-    public String getAncestors()
-    {
-        return ancestors;
-    }
+	public Long getParentId() {
+		return parentId;
+	}
 
-    public void setAncestors(String ancestors)
-    {
-        this.ancestors = ancestors;
-    }
+	public void setParentId(Long parentId) {
+		this.parentId = parentId;
+	}
 
-    public String getDeptName()
-    {
-        return deptName;
-    }
+	public String getAncestors() {
+		return ancestors;
+	}
 
-    public void setDeptName(String deptName)
-    {
-        this.deptName = deptName;
-    }
+	public void setAncestors(String ancestors) {
+		this.ancestors = ancestors;
+	}
 
-    public String getOrderNum()
-    {
-        return orderNum;
-    }
+	public String getDeptName() {
+		return deptName;
+	}
 
-    public void setOrderNum(String orderNum)
-    {
-        this.orderNum = orderNum;
-    }
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
 
-    public String getLeader()
-    {
-        return leader;
-    }
+	public String getParentDeptName() {
+		return parentDeptName;
+	}
 
-    public void setLeader(String leader)
-    {
-        this.leader = leader;
-    }
+	public void setParentDeptName(String parentDeptName) {
+		this.parentDeptName = parentDeptName;
+	}
 
-    public String getPhone()
-    {
-        return phone;
-    }
+	public String getOrderNum() {
+		return orderNum;
+	}
 
-    public void setPhone(String phone)
-    {
-        this.phone = phone;
-    }
+	public void setOrderNum(String orderNum) {
+		this.orderNum = orderNum;
+	}
 
-    public String getEmail()
-    {
-        return email;
-    }
+	public String getLeader() {
+		return leader;
+	}
 
-    public void setEmail(String email)
-    {
-        this.email = email;
-    }
+	public void setLeader(String leader) {
+		this.leader = leader;
+	}
 
-    public String getStatus()
-    {
-        return status;
-    }
+	public String getPhone() {
+		return phone;
+	}
 
-    public void setStatus(String status)
-    {
-        this.status = status;
-    }
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-    public String getDelFlag()
-    {
-        return delFlag;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setDelFlag(String delFlag)
-    {
-        this.delFlag = delFlag;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getParentName()
-    {
-        return parentName;
-    }
+	public String getStatus() {
+		return status;
+	}
 
-    public void setParentName(String parentName)
-    {
-        this.parentName = parentName;
-    }
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("deptId", getDeptId())
-            .append("parentId", getParentId())
-            .append("ancestors", getAncestors())
-            .append("deptName", getDeptName())
-            .append("orderNum", getOrderNum())
-            .append("leader", getLeader())
-            .append("phone", getPhone())
-            .append("email", getEmail())
-            .append("status", getStatus())
-            .append("delFlag", getDelFlag())
-            .append("createBy", getCreateBy())
-            .append("createTime", getCreateTime())
-            .append("updateBy", getUpdateBy())
-            .append("updateTime", getUpdateTime())
-            .toString();
-    }
+	public String getDelFlag() {
+		return delFlag;
+	}
+
+	public void setDelFlag(String delFlag) {
+		this.delFlag = delFlag;
+	}
+
+	public String getParentName() {
+		return parentName;
+	}
+
+	public void setParentName(String parentName) {
+		this.parentName = parentName;
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE).append("deptId", getDeptId())
+				.append("parentId", getParentId()).append("ancestors", getAncestors()).append("deptName", getDeptName())
+				.append("orderNum", getOrderNum()).append("leader", getLeader()).append("phone", getPhone())
+				.append("email", getEmail()).append("status", getStatus()).append("delFlag", getDelFlag())
+				.append("createBy", getCreateBy()).append("createTime", getCreateTime())
+				.append("updateBy", getUpdateBy()).append("updateTime", getUpdateTime()).toString();
+	}
 }
