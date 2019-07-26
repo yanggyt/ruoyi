e560b0dceb7f08430e533efbe20d5b188da255e3
wuminglong <mwu@wuxicloud.com>
2019-07-26 13:28:09 +0800

    将ruoyi命名去除2

---
+++ bmw-admin/src/main/java/com/bmw/web/controller/system/SysIndexController.java
@@ -0,0 +1,48 @@
+package com.bmw.web.controller.system;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.bmw.common.config.Global;
+import com.bmw.common.core.controller.BaseController;
+import com.bmw.framework.util.ShiroUtils;
+import com.bmw.system.domain.SysMenu;
+import com.bmw.system.domain.SysUser;
+import com.bmw.system.service.ISysMenuService;
+
+/**
+ * 首页 业务处理
+ * 
+ * @author ruoyi
+ */
+@Controller
+public class SysIndexController extends BaseController
+{
+    @Autowired
+    private ISysMenuService menuService;
+
+    // 系统首页
+    @GetMapping("/index")
+    public String index(ModelMap mmap)
+    {
+        // 取身份信息
+        SysUser user = ShiroUtils.getSysUser();
+        // 根据用户id取出菜单
+        List<SysMenu> menus = menuService.selectMenusByUser(user);
+        mmap.put("menus", menus);
+        mmap.put("user", user);
+        mmap.put("copyrightYear", Global.getCopyrightYear());
+        mmap.put("demoEnabled", Global.isDemoEnabled());
+        return "index";
+    }
+
+    // 系统介绍
+    @GetMapping("/system/main")
+    public String main(ModelMap mmap)
+    {
+        mmap.put("version", Global.getVersion());
+        return "main";
+    }
+}
