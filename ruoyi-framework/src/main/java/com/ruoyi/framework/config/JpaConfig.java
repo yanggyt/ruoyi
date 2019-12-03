64c083628f8ee69f91febae60baed34979b097b1
administrator <2316696377@qq.com>
2019-12-03 16:49:21 +0800

    jpa

---
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/JpaConfig.java
@@ -0,0 +1,29 @@
+package com.ruoyi.framework.config;
+
+import com.ruoyi.framework.util.ShiroUtils;
+import com.ruoyi.system.domain.SysUser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.ruoyi")
+@EnableJpaAuditing
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<Long> auditorAware(){
+        return () ->{
+            SysUser sysUser = ShiroUtils.getSysUser();
+            if(sysUser != null){
+                return Optional.of(sysUser.getUserId());
+            }
+            return Optional.empty();
+        };
+    }
+
+}
