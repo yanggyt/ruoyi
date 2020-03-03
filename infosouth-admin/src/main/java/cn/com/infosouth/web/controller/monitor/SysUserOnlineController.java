86809f8711a6bfa62274db5fcffaadcce3d8df93
tinygg <tinygg@163.com>
2020-03-04 00:02:24 +0800

    修改项目group.id和各个子module名字

---
+++ infosouth-admin/src/main/java/cn/com/infosouth/web/controller/monitor/SysUserOnlineController.java
@@ -0,0 +1,114 @@
+package cn.com.infosouth.web.controller.monitor;
+
+import java.util.List;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import cn.com.infosouth.common.annotation.Log;
+import cn.com.infosouth.common.core.controller.BaseController;
+import cn.com.infosouth.common.core.domain.AjaxResult;
+import cn.com.infosouth.common.core.page.TableDataInfo;
+import cn.com.infosouth.common.enums.BusinessType;
+import cn.com.infosouth.common.enums.OnlineStatus;
+import cn.com.infosouth.framework.shiro.session.OnlineSession;
+import cn.com.infosouth.framework.shiro.session.OnlineSessionDAO;
+import cn.com.infosouth.framework.util.ShiroUtils;
+import cn.com.infosouth.system.domain.SysUserOnline;
+import cn.com.infosouth.system.service.ISysUserOnlineService;
+
+/**
+ * 在线用户监控
+ * 
+ * @author ruoyi
+ */
+@Controller
+@RequestMapping("/monitor/online")
+public class SysUserOnlineController extends BaseController
+{
+    private String prefix = "monitor/online";
+
+    @Autowired
+    private ISysUserOnlineService userOnlineService;
+
+    @Autowired
+    private OnlineSessionDAO onlineSessionDAO;
+
+    @RequiresPermissions("monitor:online:view")
+    @GetMapping()
+    public String online()
+    {
+        return prefix + "/online";
+    }
+
+    @RequiresPermissions("monitor:online:list")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(SysUserOnline userOnline)
+    {
+        startPage();
+        List<SysUserOnline> list = userOnlineService.selectUserOnlineList(userOnline);
+        return getDataTable(list);
+    }
+
+    @RequiresPermissions("monitor:online:batchForceLogout")
+    @Log(title = "在线用户", businessType = BusinessType.FORCE)
+    @PostMapping("/batchForceLogout")
+    @ResponseBody
+    public AjaxResult batchForceLogout(@RequestParam("ids[]") String[] ids)
+    {
+        for (String sessionId : ids)
+        {
+            SysUserOnline online = userOnlineService.selectOnlineById(sessionId);
+            if (online == null)
+            {
+                return error("用户已下线");
+            }
+            OnlineSession onlineSession = (OnlineSession) onlineSessionDAO.readSession(online.getSessionId());
+            if (onlineSession == null)
+            {
+                return error("用户已下线");
+            }
+            if (sessionId.equals(ShiroUtils.getSessionId()))
+            {
+                return error("当前登陆用户无法强退");
+            }
+            onlineSession.setStatus(OnlineStatus.off_line);
+            onlineSessionDAO.update(onlineSession);
+            online.setStatus(OnlineStatus.off_line);
+            userOnlineService.saveOnline(online);
+        }
+        return success();
+    }
+
+    @RequiresPermissions("monitor:online:forceLogout")
+    @Log(title = "在线用户", businessType = BusinessType.FORCE)
+    @PostMapping("/forceLogout")
+    @ResponseBody
+    public AjaxResult forceLogout(String sessionId)
+    {
+        SysUserOnline online = userOnlineService.selectOnlineById(sessionId);
+        if (sessionId.equals(ShiroUtils.getSessionId()))
+        {
+            return error("当前登陆用户无法强退");
+        }
+        if (online == null)
+        {
+            return error("用户已下线");
+        }
+        OnlineSession onlineSession = (OnlineSession) onlineSessionDAO.readSession(online.getSessionId());
+        if (onlineSession == null)
+        {
+            return error("用户已下线");
+        }
+        onlineSession.setStatus(OnlineStatus.off_line);
+        onlineSessionDAO.update(onlineSession);
+        online.setStatus(OnlineStatus.off_line);
+        userOnlineService.saveOnline(online);
+        return success();
+    }
+}
