d1c0b1d10514e4fef4de38587b803d4cc3386363
bei.wu <5306546@qq.com>
2020-09-08 23:16:37 +0800

    修改数据库结构

--- sql/biz.sql
+++ sql/biz.sql
@@ -38,8 +38,8 @@ CREATE TABLE `biz_member_address` (
 
 CREATE TABLE `biz_account` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '会员账户ID',
-  `account_no` varchar(64) NOT NULL DEFAULT '' COMMENT '会员账户编号',
-  `account_type` tinyint(4) NOT NULL DEFAULT 0 COMMENT '账户类型：0-福豆余额，1-可用福豆，2-团队福豆，3-专项福豆，4-福豆田',
+  `member_id` bigint(20) NOT NULL COMMENT '会员ID',
+  `account_type` tinyint(4) NOT NULL DEFAULT 0 COMMENT '账户类型：0-福豆余额，1-个人福豆，2-团队福豆，3-专项福豆，4-福豆田',
   `amount` decimal(12,2) NOT NULL DEFAULT 0.0 COMMENT '账户金额',
   `create_by` varchar(64) DEFAULT '' COMMENT '创建者',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
@@ -50,10 +50,12 @@ CREATE TABLE `biz_account` (
 
 CREATE TABLE `biz_account_detail` (
    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '会员账户明细ID',
+   `member_id` bigint(20) NOT NULL COMMENT '会员ID',
    `account_id` bigint(20) NOT NULL DEFAULT 0 COMMENT '会员账户ID',
-   `account_type_cn` varchar(16) NOT NULL DEFAULT 0 COMMENT '账户类型中文',
+   `account_type` tinyint(4) NOT NULL DEFAULT 0 COMMENT '账户类型：0-福豆余额，1-个人福豆，2-团队福豆，3-专项福豆，4-福豆田',
+   `business_no` varchar(64) NOT NULL DEFAULT '' COMMENT '业务订单编号: 三方支付/兑现申请/团队明细',
    `change_type` tinyint(4) NOT NULL DEFAULT 0 COMMENT '变更类型；1:收入(加)；-1:支出(减)',
-   `type_detail` tinyint(4) NOT NULL COMMENT '变动详情.1：充值；2:提现；3:转账；',
+   `type_detail` tinyint(4) NOT NULL COMMENT '变动详情.1：充值；2:提现；3:转账；4:冲正；5:支付',
    `before_amount` decimal(12,2) NOT NULL DEFAULT 0.0 COMMENT '账户变更前金额',
    `after_amount` decimal(12,2) NOT NULL DEFAULT 0.0 COMMENT '账户变更后金额',
    `create_by` varchar(64) DEFAULT '' COMMENT '创建者',
@@ -63,27 +65,11 @@ CREATE TABLE `biz_account_detail` (
    PRIMARY KEY (`id`)
 ) COMMENT='会员账户明细表';
 
-CREATE TABLE `biz_transaction_flow` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '交易流水ID',
-  `transaction_flow_no` varchar(64) NOT NULL DEFAULT '' COMMENT '交易流水编号',
-  `business_no` varchar(64) NOT NULL DEFAULT '' COMMENT '业务订单编号',
-  `from_acc_no` varchar(64) NOT NULL DEFAULT '' COMMENT '付款方账号',
-  `from_acc_name` varchar(32) NOT NULL DEFAULT '' COMMENT '付款方名称',
-  `to_acc_no` varchar(64) NOT NULL DEFAULT '' COMMENT '收款方账号',
-  `to_acc_name` varchar(32) NOT NULL DEFAULT '' COMMENT '收款方名称',
-  `transaction_type` tinyint(4) NOT NULL DEFAULT 0 COMMENT '交易类型；1：充值；2:提现；3:转账；',
-  `transaction_amount` decimal(12,2) NOT NULL DEFAULT 0.0 COMMENT '交易金额',
-  `transaction_status` tinyint(4) NOT NULL DEFAULT 0 COMMENT '交易状态:0：初始化；1：交易成功；2：交易失败；',
-  `transaction_desc` varchar(64) DEFAULT '' COMMENT '交易备注：充值【一级推荐奖励】，充值【二级推荐奖励】，充值【团队奖励】，充值【专项划拨】，转账【专项划拨】',
-  `pay_method` varchar(2) NOT NULL DEFAULT '' COMMENT '支付方式:1:余额支付；3:支付宝；4:微信；',
-  `tripartite_pay_sn` varchar(128) NOT NULL DEFAULT '' COMMENT '第三方支付业务流水号',
-  `tripartite_callback_time` datetime DEFAULT NULL COMMENT '第三方支付业务流水号',
-  `create_by` varchar(64) DEFAULT '' COMMENT '创建者',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-  `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
-  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`)
-) COMMENT='会员账户明细表';
+-- 个人团队奖励明细表
+
+-- 三方回调表
+
+-- 兑现申请记录表
 
 CREATE TABLE `biz_product_type` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '产品分类ID',
@@ -162,3 +148,27 @@ CREATE TABLE `biz_order_detail` (
  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
  PRIMARY KEY (`id`)
 ) COMMENT='订单明细表';
+
+/**
+CREATE TABLE `biz_transaction_flow` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '交易流水ID',
+  `transaction_flow_no` varchar(64) NOT NULL DEFAULT '' COMMENT '交易流水编号',
+  `business_no` varchar(64) NOT NULL DEFAULT '' COMMENT '业务订单编号',
+  `from_acc_no` varchar(64) NOT NULL DEFAULT '' COMMENT '付款方账号',
+  `from_acc_name` varchar(32) NOT NULL DEFAULT '' COMMENT '付款方名称',
+  `to_acc_no` varchar(64) NOT NULL DEFAULT '' COMMENT '收款方账号',
+  `to_acc_name` varchar(32) NOT NULL DEFAULT '' COMMENT '收款方名称',
+  `transaction_type` tinyint(4) NOT NULL DEFAULT 0 COMMENT '交易类型；1：充值；2:提现；3:转账；4:冲正',
+  `transaction_amount` decimal(12,2) NOT NULL DEFAULT 0.0 COMMENT '交易金额',
+  `transaction_status` tinyint(4) NOT NULL DEFAULT 0 COMMENT '交易状态:0：初始化；1：交易成功；2：交易失败；',
+  `transaction_desc` varchar(64) DEFAULT '' COMMENT '交易备注：充值【一级推荐奖励】，充值【二级推荐奖励】，充值【团队奖励】，充值【专项划拨】，转账【专项划拨】',
+  `pay_method` varchar(2) NOT NULL DEFAULT '' COMMENT '支付方式:1:余额支付；3:支付宝；4:微信；',
+  `tripartite_pay_sn` varchar(128) NOT NULL DEFAULT '' COMMENT '第三方支付业务流水号',
+  `tripartite_callback_time` datetime DEFAULT NULL COMMENT '第三方支付回调时间',
+  `create_by` varchar(64) DEFAULT '' COMMENT '创建者',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) COMMENT='会员账户明细表';
+ */
