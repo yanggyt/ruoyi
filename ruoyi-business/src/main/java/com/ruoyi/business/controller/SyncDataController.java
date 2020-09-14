7bfa938679afcc9ff71092221ca1f1ac1dd7826c
bei.wu <5306546@qq.com>
2020-09-14 14:29:20 +0800

    添加商品分类初始化数据

--- ruoyi-business/src/main/java/com/ruoyi/business/controller/SyncDataController.java
+++ ruoyi-business/src/main/java/com/ruoyi/business/controller/SyncDataController.java
@@ -2,6 +2,7 @@ package com.ruoyi.business.controller;
 
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelReader;
+import com.ruoyi.business.service.IBizMemberService;
 import com.ruoyi.business.sync.UserData;
 import com.ruoyi.business.sync.UserDataListener;
 import com.ruoyi.common.core.controller.BaseController;
@@ -12,17 +13,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/data")
 public class SyncDataController extends BaseController {
 
+    @Resource
+    private IBizMemberService bizMemberService;
+
     @PostMapping("/user")
     public AjaxResult user(@RequestParam("file") MultipartFile file) {
         ExcelReader reader = null;
         try {
-            reader = EasyExcel.read(file.getInputStream(), UserData.class, new UserDataListener()).build();
+            reader = EasyExcel.read(file.getInputStream(), UserData.class, new UserDataListener(bizMemberService)).build();
             reader.readAll();
         } catch (IOException e) {
             e.printStackTrace();
--- ruoyi-business/src/main/java/com/ruoyi/business/sync/UserDataListener.java
+++ ruoyi-business/src/main/java/com/ruoyi/business/sync/UserDataListener.java
@@ -2,11 +2,29 @@ package com.ruoyi.business.sync;
 
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
+import com.ruoyi.business.domain.BizMember;
+import com.ruoyi.business.service.IBizMemberService;
+import com.ruoyi.common.utils.DateUtils;
 
 public class UserDataListener extends AnalysisEventListener<UserData> {
 
+    private IBizMemberService bizMemberService;
+
+    public UserDataListener(IBizMemberService bizMemberService) {
+        this.bizMemberService = bizMemberService;
+    }
+
     @Override
     public void invoke(UserData userData, AnalysisContext analysisContext) {
+        BizMember member = new BizMember();
+        member.setMemberName(userData.getName());
+        member.setPassword(userData.getPassword());
+        member.setIsDelete(0);
+        member.setIsEnable(Integer.parseInt(userData.getEnable()));
+        member.setCreateTime(DateUtils.parseDate(userData.getAddtim()));
+        member.setMobile(userData.getReferrerTelephone());
+        member.setRecommendMobile(userData.getReferrerTelephone());
+        member.setRecommendName(userData.getReferrer());
         System.out.println(userData);
     }
 
