f1c70f4cb11c2fd96969e79391a9befbc0d59ae2
wangwu <wangwu@haier.com>
2020-09-17 18:38:03 +0800

    开发DOE分析功能

--- ruoyi-admin/src/main/java/com/ruoyi/dfm/controller/DoeAnalysisController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/dfm/controller/DoeAnalysisController.java
@@ -1,15 +1,21 @@
 package com.ruoyi.dfm.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.common.utils.http.HttpUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
 
 
 /**
@@ -23,8 +29,16 @@ public class DoeAnalysisController extends BaseController {
 
     private static final Logger log = LoggerFactory.getLogger(DoeAnalysisController.class);
 
+    private static final String DATA_TYPE_KEY_PARAM = "keyParam";
+    private static final String DATA_TYPE_REWORK = "rework";
+
+    private static final Integer SUCCESS_CODE = new Integer(20);
+
     @Value("${api.doe.GetDataByKeyParam}")
     private String getDataByKeyParamUrl;
+
+    @Value("${api.doe.GetDataByRework}")
+    private String getDataByReworkUrl;
 
     /**
      * 获取分析页面
@@ -45,12 +59,28 @@ public class DoeAnalysisController extends BaseController {
     @PostMapping("/list")
     @ResponseBody
     public TableDataInfo list(@RequestParam("productname") String productname, @RequestParam("version") String version, @RequestParam("dataType") String dataType) {
-        String apiUrl = apiRootUrl + getDataByKeyParamUrl;
+        String apiUrl = apiRootUrl;
         String param = "productname=" + productname +"&version=" + version;
+        if(DATA_TYPE_KEY_PARAM.equals(dataType)) {
+            apiUrl += getDataByKeyParamUrl;
+        } else if(DATA_TYPE_REWORK.equals(dataType)) {
+            apiUrl += getDataByReworkUrl;
+        }
         log.info("request remote api, url={}, param={}", apiUrl, param);
         String result = HttpUtils.sendGet(apiUrl, param);
         log.info("response remote api, url={}, param={}, result={}", apiUrl, param, result);
-        return getDataTable(null);
+        if(StringUtils.isBlank(result)) {
+            log.error("reponse result is null. url={}, param={}", apiUrl, param);
+            return getDataTable(Collections.emptyList());
+        }
+        JSONObject resultObj = JSON.parseObject(result);
+        if(null != resultObj && SUCCESS_CODE.equals(resultObj.getInteger("code"))) {
+            JSONArray data = resultObj.getJSONArray("data");
+            return getDataTable(data);
+        } else {
+            log.error("reponse result failed. url={}, param={}, result={}", apiUrl, param, result);
+        }
+        return getDataTable(Collections.emptyList());
     }
 
 }
