4f2a32e733d0a73ff719c07713c36e8730983991
wsy <28511238@qq.com>
2020-09-25 09:43:58 +0800

    Revert "增加分页参数默认值，防止传空时查询全表"
    This reverts commit 99e7820bc74818d8f0b97d865db3f65761e7424a.

--- ruoyi-common/src/main/java/com/ruoyi/common/core/controller/BaseController.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/controller/BaseController.java
@@ -56,8 +56,11 @@ public class BaseController
         PageDomain pageDomain = TableSupport.buildPageRequest();
         Integer pageNum = pageDomain.getPageNum();
         Integer pageSize = pageDomain.getPageSize();
-        String orderBy = SqlUtil.escapeOrderBySql(pageDomain.getOrderBy());
-        PageHelper.startPage(pageNum, pageSize, orderBy);
+        if (StringUtils.isNotNull(pageNum) && StringUtils.isNotNull(pageSize))
+        {
+            String orderBy = SqlUtil.escapeOrderBySql(pageDomain.getOrderBy());
+            PageHelper.startPage(pageNum, pageSize, orderBy);
+        }
     }
 
     /**
