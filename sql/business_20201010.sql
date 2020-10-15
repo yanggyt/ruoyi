a8990d3645b664030698732aab6c83a4ddc75298
dingruichao <281809423@qq.com>
2020-10-15 21:48:05 +0800

    首页轮播图表

--- sql/business_20201010.sql
+++ sql/business_20201010.sql
@@ -242,3 +242,18 @@ create table event_recruitment (
   update_time  datetime  comment '更新时间',
   primary key (id)
 ) engine=innodb auto_increment=10 comment = '活动招募表';
+
+-- ----------------------------
+-- 13、首页轮播图表
+-- ----------------------------
+drop table if exists home_page_carousel;
+create table home_page_carousel (
+  id  bigint(20) not null auto_increment  comment 'ID',
+  picture_url  varchar(120) not null comment '图片地址',
+  del_flag  char(1)  default '0' comment '删除标志（0代表存在 2代表删除）',
+  create_by  varchar(64)  default '' comment '创建者',
+  create_time  datetime    comment '创建时间',
+  update_by  varchar(64)  default ''  comment '更新者',
+  update_time  datetime  comment '更新时间',
+  primary key (id)
+) engine=innodb auto_increment=10 comment = '首页轮播图表';
