ca3924520dc21ba45b6b0eaa36cf428793b22534
zkr_liushenlu <liushenlu@sinosoft.com.cn>
2021-03-23 16:28:06 +0800

    广告控制器

---
+++ ruoyi-content/src/main/java/com/ruoyi/content/controller/AdvertisementController.java
@@ -0,0 +1,16 @@
+package com.ruoyi.content.controller;
+
+import com.ruoyi.common.core.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @program: ruoyi->AdvertisementController
+ * @description: 广告
+ * @author: LiuShenlu
+ * @create: 2021-03-23 16:26
+ **/
+@Controller
+@RequestMapping("advert")
+public class AdvertisementController extends BaseController {
+}
