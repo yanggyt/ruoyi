5ae2295e950207fb32336c364e55ffcd5915d919
Bo <septemyang@163.com>
2021-08-18 15:15:35 +0000

    删除文件 box-test/src/main/java/com/ruoyi/test/service/TestVerifyService.java

--- box-test/src/main/java/com/ruoyi/test/service/TestVerifyService.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.test.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface TestVerifyService {
-    //检查姓名是否唯一
-    int isNameUnique(String name);
-}
